bc4fcaa8070beed679e1bc74b70ee51e1ac77c9c
Phung Quoc Luong <quocluong2503@gmail.com>
2021-12-16 13:26:37 +0700

    chinh sua bang diem

--- src/containers/GradeTable/GradeTable.jsx
+++ src/containers/GradeTable/GradeTable.jsx
@@ -1,35 +1,92 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SpinnerWrapper } from '../../components/common';
 import { DataGrid } from '@mui/x-data-grid';
 import { apiClasses } from '../../services/api';
+import { useParams } from 'react-router-dom';
 
 export const GradeTable = () => {
     const [loading, setLoading] = useState(false)
-
+    let { id } = useParams();
     const [editRowsModel, setEditRowsModel] = React.useState({});
-
     const handleEditRowsModelChange = React.useCallback((model) => {
         setEditRowsModel(model);
     }, []);
+    const [score, setScore] = useState(
+        [
+            {
+                id: 18120458,
+                name: "luong",
+                grade1: 10,
+                grade2: 9
+            },
+            {
+                id: 18120443,
+                name: "long",
+                grade1: 10,
+                grade2: 9
+            },
+            {
+                id: 18120502,
+                name: "phat",
+                grade1: 10,
+                grade2: 9
+            },
+        ]
+    )
+
+    const [col, setCol] = useState([
+        { 
+            field: 'id', 
+            type: 'number', 
+            editable: false, 
+            width: 200,
+            renderCell: ({value}) => (<div>{value}</div>),
+            renderHeader: () => (<div> studentId aa</div>)
+            // valueGetter: getFullName,
+        },
+        { 
+            field: 'name', 
+            headerName: 'Name', 
+            width: 200, 
+            editable: false
+        }
+    ])
+
+    useEffect(() => {
+        setLoading(true);
+        getInfor(id);
+    }, [])
+
+    const getInfor = async (id) => {
+        let d = await apiClasses.getClassDetail(id);
+        if (d && d.data) {
+            let temp = (JSON.parse(JSON.parse(d.data?.gradeStructure)) ?? []);
+            setCol([...col, ...temp])
+        }
+        setLoading(false);
+      };
+
+
 
     // const getFullName = (params) => {
     //     return `${params.row.firstName || ''} ${params.row.lastName || ''}`;
     //   }
 
 
-    const columns = [
-        { 
-            field: 'id', 
-            type: 'number', 
-            editable: false, 
-            renderCell: ({value}) => (<div>{value}</div>),
-            renderHeader: () => (<div> studentId aa</div>)
-            // valueGetter: getFullName,
-        },
-        { field: 'name', headerName: 'Name', width: 180, editable: false },
-        { field: 'grade1', headerName: 'bt1', width: 180, editable: true },
-        { field: 'grade2', headerName: 'bt2', width: 180, editable: true }
-    ];
+    // const columns = [
+    //     { 
+    //         field: 'id', 
+    //         type: 'number', 
+    //         editable: false, 
+    //         width: 200,
+    //         renderCell: ({value}) => (<div>{value}</div>),
+    //         renderHeader: () => (<div> studentId aa</div>)
+    //         // valueGetter: getFullName,
+    //     },
+    //     { field: 'name', headerName: 'Name', width: 200, editable: false },
+    //     { field: 'grade1', headerName: 'bt1', width: 200, editable: true },
+    //     { field: 'grade2', headerName: 'bt2', width: 200, editable: true }
+    // ];
 
     const rows = [
         {
@@ -55,8 +112,9 @@ export const GradeTable = () => {
             <div style={{ width: '100%' }}>
                 <div style={{ height: 400, width: '100%' }}>
                     <DataGrid
+                        pagination={false}
                         rows={rows}
-                        columns={columns}
+                        columns={col}
                         editRowsModel={editRowsModel}
                         editMode="row"
                         onEditRowsModelChange={handleEditRowsModelChange}
